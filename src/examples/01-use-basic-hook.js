5f1aab87a2826362289e851d26e0e4823bd9d60c
Hans Garcia <hanslgarcia@gmail.com>
2018-11-07 22:51:37 -0500

    feat: implemented react hook to increment a counter

--- src/examples/01-use-basic-hook.js
+++ src/examples/01-use-basic-hook.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Counter() {
+  const [counter, setCounter] = useState(0);
+  const IncrementCount = () => {
+    setCounter(currentCount => {
+      return currentCount + 1;
+    });
+  };
+
   return (
     <div>
-      <button>{0}</button>
+      <button onClick={IncrementCount}>{counter}</button>
     </div>
   );
 }
