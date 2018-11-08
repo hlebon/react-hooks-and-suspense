eedd4fc3b3f6517de7616ddf375110660ae09bfe
Hans Garcia <hanslgarcia@gmail.com>
2018-11-07 23:35:01 -0500

    feat: how to test react state hook

--- src/examples/01-use-basic-hook.js
+++ src/examples/01-use-basic-hook.js
@@ -1,5 +1,9 @@
 import React, { useState } from "react";
 
+// const [count, setCount] = useState(0)
+// const incrementCount = () => setCount(currentCount => currentCount + 1)
+// return <button onClick={incrementCount}>{count}</button>
+
 function Counter() {
   const [counter, setCounter] = useState(0);
   const IncrementCount = () => {
@@ -8,11 +12,7 @@ function Counter() {
     });
   };
 
-  return (
-    <div>
-      <button onClick={IncrementCount}>{counter}</button>
-    </div>
-  );
+  return <button onClick={IncrementCount}>{counter}</button>;
 }
 
 export default Counter;
